c8d66479a50cc4fe66d6cb3f803926aedc53ac3b
naufal.azwar <naufal.azwar@traveloka.com>
2023-09-30 15:19:24 +0700

    [search-by-keyword] adding new component to handle search by keyword

--- src/app/components/product-list/product-list.component.ts
+++ src/app/components/product-list/product-list.component.ts
@@ -12,6 +12,7 @@ export class ProductListComponent implements OnInit {
 
   products: Product[] = [];
   currentCategoryId: number = 1;
+  searchMode: boolean = false;
 
   constructor(private productService: ProductService, 
               private route: ActivatedRoute) { }
@@ -24,6 +25,29 @@ export class ProductListComponent implements OnInit {
 
   listProducts() {
 
+    this.searchMode = this.route.snapshot.paramMap.has('keyword');
+
+    if (this.searchMode) {
+      this.handleSearchProducts();
+    } else {
+      this.handleListProducts();
+    }
+
+  }
+
+  handleSearchProducts() {
+
+    const theKeyword: string = this.route.snapshot.paramMap.get('keyword')!;
+
+    // now search for the products using keyword
+    this.productService.searchProducts(theKeyword).subscribe(
+      data => {
+        this.products = data;
+      }
+    );
+  }
+
+  handleListProducts() {
     // check if id parameter is available
     const hasCategoryId: boolean = this.route.snapshot.paramMap.has('id');
 
@@ -35,6 +59,7 @@ export class ProductListComponent implements OnInit {
       this.currentCategoryId = 1;
     }
 
+    // now get the products for the given category id
     this.productService.getProductList(this.currentCategoryId).subscribe(
       data => {
         this.products = data;
