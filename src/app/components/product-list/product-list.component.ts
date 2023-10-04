9419f8020ab4f5bbad65a08afc84de0b98d66712
naufal.azwar <naufal.azwar@traveloka.com>
2023-10-04 22:57:14 +0700

    [pagination] adding pagination

--- src/app/components/product-list/product-list.component.ts
+++ src/app/components/product-list/product-list.component.ts
@@ -12,7 +12,13 @@ export class ProductListComponent implements OnInit {
 
   products: Product[] = [];
   currentCategoryId: number = 1;
+  previousCategoryId: number = 1;
   searchMode: boolean = false;
+
+  // new properties for pagination
+  thePageNumber: number = 1;
+  thePageSize: number = 10;
+  theTotalElements: number = 0;
 
   constructor(private productService: ProductService, 
               private route: ActivatedRoute) { }
@@ -59,12 +65,33 @@ export class ProductListComponent implements OnInit {
       this.currentCategoryId = 1;
     }
 
-    // now get the products for the given category id
-    this.productService.getProductList(this.currentCategoryId).subscribe(
-      data => {
-        this.products = data;
-      }
-    )
-  }
+    //
+    // Check if we have a different category than the previous one
+    // Note: Angular will reuse a component if it is currently being viewed
+    //
 
+    // if we have a different category id than the previous one,
+    // then set thePageNumber back to 1
+    if (this.previousCategoryId != this.currentCategoryId) {
+      this.thePageNumber = 1;
+    }
+
+    this.previousCategoryId = this.currentCategoryId;
+
+    console.log(`currentCategoryId=${this.currentCategoryId}, thePageNumber=${this.thePageNumber}`);
+
+    // now get the products for the given category id
+    this.productService.getProductListPaginate(this.thePageNumber - 1,
+                                               this.thePageSize, 
+                                               this.currentCategoryId)
+                                               .subscribe(
+                                                data => {
+                                                  this.products = data._embedded.products;
+                                                  this.thePageNumber = data.page.number + 1;
+                                                  this.thePageSize = data.page.size;
+                                                  this.theTotalElements = data.page.totalElements;
+                                                }
+                                               );
+
+  }
 }
