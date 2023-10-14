573d36a093eba3deb501d6cb67c5bcd07a125431
naufal.azwar <naufal.azwar@traveloka.com>
2023-10-15 00:42:29 +0700

    [login] integrate to okta

---
+++ src/app/config/my-app-config.ts
@@ -0,0 +1,9 @@
+export default {
+
+    oidc: {
+        clientId: '0oacrkblnwNPRIfOI5d7',
+        issuer: 'https://dev-75915139.okta.com/oauth2/default',
+        redirectUri: 'http://localhost:4200/login/callback',
+        scopes: ['openid', 'profile', 'email']
+    }
+}
